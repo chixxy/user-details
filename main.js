54e8ff60dec5450cb4f32cbbce7ea7b15e743613
chixxy <chiziegwunwankwo@gmail.com>
2022-06-23 10:00:16 +0100

    it shows user details on screen

---
+++ main.js
@@ -0,0 +1,7 @@
+let nam = "Egwunwankwo Chizitere";
+let height = "5'10";
+let country = "Nigeria";
+
+alert(`My name is ${nam}`);
+alert(`My height is ${height}`);
+alert(`My country is ${country}`);
